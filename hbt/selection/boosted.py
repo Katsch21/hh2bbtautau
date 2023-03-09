d9749a48a70d5d621566b9198cb5bb077017563d
Katharina Margarethe Herges <hergesk@naf-cms14.desy.de>
2023-03-09 13:03:08 +0100

    started implementation of gen matching to HH decay products

--- hbt/selection/boosted.py
+++ hbt/selection/boosted.py
@@ -16,10 +16,12 @@ from operator import and_
 from functools import reduce
 
 from hbt.selection.boosted_jet import boosted_jet_selector
+from hbt.selection.jet import jet_selection
 from hbt.selection.lepton import lepton_selection
 from hbt.selection.trigger import trigger_selection
 from hbt.selection.default import increment_stats
 from hbt.production.gen_HH_decay import gen_HH_decay_products
+from hbt.selection.genmatching import genmatching_selector
 from IPython import embed
 
 np = maybe_import("numpy")
@@ -31,16 +33,17 @@ ak = maybe_import("awkward")
     uses={boosted_jet_selector, lepton_selection, trigger_selection,
         process_ids, increment_stats, attach_coffea_behavior,
         btag_weights, pu_weight, mc_weight, pdf_weights, murmuf_weights,
-        cutflow_features, gen_HH_decay_products, "nGenJet", "GenJet.*",
-        "nGenVisTau", "GenVisTau.*"
+        cutflow_features, gen_HH_decay_products, genmatching_selector,
+        jet_selection
           },
     produces={
         trigger_selection, lepton_selection, boosted_jet_selector,
         process_ids, increment_stats, attach_coffea_behavior,
         btag_weights, pu_weight, mc_weight, pdf_weights, murmuf_weights,
         cutflow_features,
+        jet_selection
     },
-    sandbox=dev_sandbox("bash::$HBT_BASE/sandboxes/venv_columnar.sh"),
+    sandbox=dev_sandbox("bash::$HBT_BASE/sandboxes/venv_columnar_tf.sh"),
     exposed=True,
 )
 def boosted(
@@ -67,7 +70,7 @@ def boosted(
     results += lepton_results
 
     # fat jet selections
-    events, jet_results = self[boosted_jet_selector](events, trigger_results, lepton_results, **kwargs)
+    events, jet_results = self[jet_selection](events, trigger_results, lepton_results, **kwargs)
     results += jet_results
 
     # mc-only functions
@@ -84,6 +87,7 @@ def boosted(
         # btag weights
         events = self[btag_weights](events, results.x.jet_mask, **kwargs)
 
+    # embed()
     event_sel = reduce(and_, results.steps.values())
     results.main["event"] = event_sel
 
@@ -103,12 +107,42 @@ def boosted(
 
     events = self[gen_HH_decay_products](events, **kwargs)
 
-    # match genJets to genPartons from H
-    # get GenJets with b as partonFlavour
-    genBjets = events.GenJet[abs(events.GenJet.partonFlavour) == 5]
+    events = self[genmatching_selector](events, jet_collection=events.Jet, jet_results=jet_results)
 
-    # calculate deltaR between genBjets and gen b partons from H
-    metric = genBjets.metric_table(events.genBpartonH)
-    from IPython import embed; embed()
+    # already in genmatching.py:
+    
+    # # match genJets to genPartons from H
+    # # get GenJets with b as partonFlavour
+    # genBjets = events.GenJet[abs(events.GenJet.partonFlavour) == 5]
+
+    # # calculate deltaR between genBjets and gen b partons from H
+    # nearest_genjet_to_parton = events.genBpartonH.nearest(genBjets, threshold=0.4)
+
+    # # filter unmatched cases
+    # unmatched_genjets = ak.is_none(nearest_genjet_to_parton.pt, axis=1)
+
+    # matched_genjets_to_parton = nearest_genjet_to_parton[~unmatched_genjets]
+
+    # # calculated deltaR between matched Gen Jets and Jets
+    # nearest_jets_to_genjets = matched_genjets_to_parton.nearest(events.Jet, threshold=0.4)
+    
+    # metrics_jets_to_genjets = matched_genjets_to_parton.metric_table(events.Jet, axis=1)
+    
+    # mmin = ak.argmin(metrics_jets_to_genjets, axis=2, keepdims=True)
+    # metric = ak.firsts(metrics_jets_to_genjets[mmin], axis=2)
+    
+    # mmin = ak.firsts(mmin.mask[metric <= 0.4], axis=2)
+    
+    
+
+
+    # # filter unmatched cases
+    # unmatched_jets = ak.is_none(nearest_jets_to_genjets.pt, axis=1)
+
+    # matched_jets_to_genjets = nearest_jets_to_genjets[~unmatched_jets]
+
+
+
+
 
     return events, results
