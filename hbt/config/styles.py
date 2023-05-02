dc8d93f7f4791a5e053b8f63659297f493990ce7
Katharina Margarethe Herges <hergesk@naf-cms17.desy.de>
2023-05-02 19:21:27 +0200

    added different colours for different datasets.

--- hbt/config/styles.py
+++ hbt/config/styles.py
@@ -14,6 +14,34 @@ def stylize_processes(config: od.Config) -> None:
     if config.has_process("hh_ggf_bbtautau"):
         config.processes.n.hh_ggf_bbtautau.color1 = (67, 118, 201)
 
+    # process colours for my datasets
+    # as I do not plot any background, I can just use the whole colour range
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m400"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m400.color1 = (255, 0, 0)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m800"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m800.color1 = (255, 0, 136)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m1000"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m1000.color1 = (166, 0, 255)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m1250"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m1250.color1 = (0, 60, 255)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m1750"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m1750.color1 = (0, 200, 255)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m2000"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m2000.color1 = (75, 55, 189)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m2500"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m2500.color1 = (0, 255, 128)
+
+    if config.has_process("graviton_hh_ggf_bbtautau_m3000"):
+            config.processes.n.graviton_hh_ggf_bbtautau_m3000.color1 = (255, 145, 0)
+
+
     if config.has_process("h"):
         config.processes.n.h.color1 = (65, 180, 219)
 
