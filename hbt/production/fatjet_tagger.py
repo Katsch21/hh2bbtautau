7bf924a4181f5e009fbd9763c5bc03067215040f
Katharina Margarethe Herges <hergesk@naf-cms15.desy.de>
2023-06-05 11:28:59 +0200

    fixed error for fatjet tagging variables

--- hbt/production/fatjet_tagger.py
+++ hbt/production/fatjet_tagger.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 """
-Producers that determine the generator-level particles related to a top quark decay.
+Creates columns for different AK8 Jet discriminators X:
+    - HardestFatJet.X: Creates columns for discriminator value of hardest jet
+    - FirstFatJet.X: Creates columns for highest discriminator value 
 """
 
 from columnflow.production import Producer, producer
@@ -9,32 +11,29 @@ from columnflow.util import maybe_import, dev_sandbox
 from columnflow.columnar_util import EMPTY_FLOAT, layout_ak_array, Route
 from columnflow.columnar_util import set_ak_column
 
+from IPython import embed
+
 
 ak = maybe_import("awkward")
 
 @producer(
     fatjet_observables = {
-        "btagCSVV2", "btagDeepB", "pt"
-        # "deepTagMD_H4qvsQCD",
-        # "deepTagMD_HbbvsQCD", "deepTagMD_ZHbbvsQCD", "deepTag_H",
-        # "particleNetMD_Xbb", "particleNet_HbbvsQCD", "btagHbb"
+        "btagCSVV2", "btagDeepB", "pt",
+        "deepTagMD_H4qvsQCD",
+        "deepTagMD_HbbvsQCD", "deepTagMD_ZHbbvsQCD", "deepTag_H",
+        "particleNetMD_Xbb", "particleNet_HbbvsQCD", "btagHbb",
     },
+    four_momenta_vars = {"pt", "phi", "eta", "mass"},
     sandbox=dev_sandbox("bash::$HBT_BASE/sandboxes/venv_columnar.sh"),
 )
 def fatjet_tagging_variables(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     """
-    Creates a new ragged column "gen_top_decay" with one element per hard top quark. Each element is
-    a GenParticleArray with five or more objects in a distinct order: top quark, bottom quark,
-    W boson, down-type quark or charged lepton, up-type quark or neutrino, and any additional decay
-    produces of the W boson (if any, then most likly photon radiations). Per event, the structure
-    will be similar to:
-
+    Creates columns for HardestFatJet.X and FirstFatJet.X
     """
     # sort for fatjet pt
-
+    
     pt_indices = ak.argsort(events.FatJet.pt, axis=1, ascending=False)
-    from IPython import embed
-    embed()
+    # embed()
     for var in self.fatjet_observables:
         var_indices = ak.argsort(Route(f"{var}[...]").apply(events.FatJet, EMPTY_FLOAT), ascending=False)
         events = set_ak_column(
@@ -45,6 +44,17 @@ def fatjet_tagging_variables(self: Producer, events: ak.Array, **kwargs) -> ak.A
             events,
             f"FirstFatJet.{var}",
             Route(f"{var}[:,0]").apply(events.FatJet[var_indices], EMPTY_FLOAT))
+        for four_mom_var in self.four_momenta_vars:
+            events = set_ak_column(
+                events,
+                f"FirstFatJet_{var}.{four_mom_var}",
+                Route(f"{four_mom_var}[:,0]").apply(events.FatJet[var_indices], EMPTY_FLOAT))
+    
+    for var in self.four_momenta_vars:
+        events = set_ak_column(
+            events,
+            f"HardestFatJet.{var}",
+            Route(f"{var}[:,0]").apply(events.FatJet[pt_indices], EMPTY_FLOAT))
 
     return events
 
@@ -56,5 +66,11 @@ def fatjet_tagging_variables_init(self: Producer) -> None:
     a dataset including top decays is processed.
     """
     self.uses |= {f"FatJet.{x}" for x in self.fatjet_observables}
+    self.uses |= {f"FatJet.{x}" for x in self.four_momenta_vars}
+    #print("fatjet_tagging_init")
+    #embed()
     self.produces |= {f"HardestFatJet.{x}" for x in self.fatjet_observables}
     self.produces |= {f"FirstFatJet.{x}" for x in self.fatjet_observables}
+    self.produces |= {f"HardestFatJet.{x}" for x in self.four_momenta_vars}
+    self.produces |= {f"FirstFatJet.{x}" for x in self.four_momenta_vars}
+
