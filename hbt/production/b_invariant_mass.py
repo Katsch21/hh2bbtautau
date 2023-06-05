237b50fed8266b160d13791b3d6628bbacc04c8a
Katharina Margarethe Herges <hergesk@naf-cms15.desy.de>
2023-06-05 11:22:47 +0200

    solved problem for invariant mass plotting

--- hbt/production/b_invariant_mass.py
+++ hbt/production/b_invariant_mass.py
@@ -19,6 +19,26 @@ np = maybe_import("numpy")
 set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
 set_ak_column_i32 = functools.partial(set_ak_column, value_type=np.int32)
 
+def calculate_mass(array: ak.Array, n_objects: int=2):
+    # now add the four momenta of the first two `Jet`s using the
+    # `sum` function of coffea's LorentzVector class
+    if n_objects > 1:
+        dijets = array[:, :n_objects].sum(axis=1)
+        # the `add` operation is only meaningful if we have at leas two
+        # jets, so create a selection accordingly
+        dijet_mask = ak.num(array, axis=1) >= n_objects
+    elif n_objects == 1:
+        dijets = array
+        dijet_mask = ak.full_like(array, True)
+
+    # now add the new column to the array *events*. The column contains
+    # the mass of the summed four momenta where there are at least
+    # two jets, and the default value `EMPTY_FLOAT` otherwise.
+    # from IPython import embed
+    # embed()
+    col = ak.where(dijet_mask, dijets.mass, EMPTY_FLOAT)
+    return col
+
 @producer(
     uses={
        "GenmatchedHHBtagJets.*", "Jet.*", "HHBJet.*",
@@ -29,7 +49,7 @@ set_ak_column_i32 = functools.partial(set_ak_column, value_type=np.int32)
         "HHBJet_dijet_mass",
     },
 )
-def invariant_mass(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+def invariant_mass_hhbjet(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     """
     Creates new column for the invariant mass of selected bjets.
     """
@@ -38,24 +58,45 @@ def invariant_mass(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     collections = {x: {"type_name" : "Jet"} for x in ["HHBJet"]}
     events = self[attach_coffea_behavior](events, collections=collections, **kwargs)
 
-    def calculate_mass(array: ak.Array):
-        # now add the four momenta of the first two `Jet`s using the
-        # `sum` function of coffea's LorentzVector class
-        dijets = array[:, :2].sum(axis=1)
-
-        # the `add` operation is only meaningful if we have at leas two
-        # jets, so create a selection accordingly
-        dijet_mask = ak.num(array, axis=1) >= 2
-
-        # now add the new column to the array *events*. The column contains
-        # the mass of the summed four momenta where there are at least
-        # two jets, and the default value `EMPTY_FLOAT` otherwise.
-        col = ak.where(dijet_mask, dijets.mass, EMPTY_FLOAT)
-        return col
+    
 
     events.set_ak_column_f32(
             events,
             "HHBJet_dijet_mass",
             calculate_mass(events.HHBJet)
     )
+    return events
+
+
+@producer(
+    uses={
+       "FatJet.*", "HardestFatJet.*", "FirstFatJet.*",
+        attach_coffea_behavior
+    },
+    produces={
+        # new columns
+        "HardestFatJet_disubjets_mass", "FirstFatJet_disubjets_mass",
+        "HardestFatJet.mass", "FirstFatJet.mass",
+    },
+)
+def invariant_mass_fatjets(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+    """
+    Creates new column for the invariant mass of selected bjets.
+    """
+    # attach four momenta behavior for the field `Jet`
+    # for more information see documentation of attach_coffea_behavior
+    collections = {x: {"type_name" : "FatJet"} for x in ["HardestFatJet", "FirstFatJet"]}
+    events = self[attach_coffea_behavior](events, collections=collections, **kwargs)
+
+    set_ak_column_f32(
+            events,
+            "HardestFatJet.mass",
+            calculate_mass(events.HardestFatJet, n_objects=1)
+    )
+    # set_ak_column_f32(
+    #         events,
+    #         "FirstFatJet.mass",
+    #         calculate_mass(events.FirstFatJet, n_objects=1)
+    # )
+
     return events
