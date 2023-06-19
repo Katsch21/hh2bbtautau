b5870c334ccb7e10cd451dfd7fa9bab73556201a
Katharina Margarethe Herges <hergesk@naf-cms15.desy.de>
2023-06-19 12:41:23 +0200

    added new categories

--- hbt/config/categories.py
+++ hbt/config/categories.py
@@ -27,3 +27,38 @@ def add_categories(config: od.Config) -> None:
         selection="sel_2j",
         label="2 jets",
     )
+    add_category(
+        config,
+        name="ak4_sel",
+        id=101,
+        selection="sel_incl",
+        label=r"$n_{jets, AK4} \geq 2$",
+    )
+    add_category(
+        config,
+        name="ak8_sel",
+        id=102,
+        selection="sel_incl",
+        label=r"$n_{jets, AK4} < 2$",
+    )
+    add_category(
+        config,
+        name="ak8_sel_fatjet_tag",
+        id=103,
+        selection="sel_incl",
+        label=r"$n_{jets, AK4} < 2$, AK8 jet tagging",
+    )
+    add_category(
+        config,
+        name="ak8_sel_subjet_tag",
+        id=104,
+        selection="sel_incl",
+        label=r"$n_{jets, AK4} < 2$, Subjet tagging",
+    )
+    add_category(
+        config,
+        name="complete_phasespace",
+        id=105,
+        selection="sel_incl",
+        label=r"Entire phase space",
+    )
